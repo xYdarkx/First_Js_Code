de2c9ca6a9aef44f11ee94d328c02517e4f50a5e
xYdarkx <64287637+xYdarkx@users.noreply.github.com>
2020-04-25 21:01:26 +0200

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,44 @@
+// I have never coded in JS before so I did some research and I used my intuation xD
+// Test functions area
+var A,B,Op;
+function OperatorTest(Op,A,B){
+    var result;
+    if (Op == '+'){
+        result = A + B;
+    }
+    else if (Op == '-'){
+        result = A - B;
+    }
+    else if (Op == '*'){
+        result = A * B;
+    }
+    else if (Op == '/'){
+        result = A / B;
+    }
+    console.log("The result is "+ result);
+}
+function Welcome_and_input(){
+    console.log('Hello,I am your calculator');
+     A = prompt("Give me the first number");
+     A = parseInt(A);
+     B = prompt("Give me the second number");
+    B = parseInt(B);
+     Op = prompt("What is the operator needed?");
+}
+function Power(A,B){
+    var i; var p =1;
+    for (i=1;i<=B; i++){
+        p= p*A;
+    }
+    console.log(A+" Power "+B+ " = "+ p);
+}
+
+function Calculator(){
+    Welcome_and_input();
+    OperatorTest(Op,A,B);
+    Power(A,B);
+    Power(B,A);
+    }
+
+Calculator();
+
